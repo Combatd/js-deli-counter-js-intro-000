247c0af3b72d15a1586881494950d1b54538ad86
Mark Calvelo <combatdstar@gmail.com>
2019-08-28 15:58:40 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+// customer enters the deli
+function takeANumber(people, name) {
+  people.push(name); // add person to the line
+  return `Welcome, ${name}. You are number ${people.length} in line.`;
+}
+
+// serve the first person in line
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length < 1) {
+    return "There is nobody waiting to be served!"; // nobody in line
+  }
+  
+  return `Currently serving ${katzDeliLine[0]}.`; 
+}
+
+function currentLine(line) {
+  if (line.length < 1) {
+    return "The line is currently empty.";
+  }
+  
+  let prompt = `The line is currently:`;
+  let i = 0
+  while (i < line.length) {
+    prompt += ` ${ line[i]}`;
+    i++
+  }
+  prompt += '.';
+  return prompt;
+}
