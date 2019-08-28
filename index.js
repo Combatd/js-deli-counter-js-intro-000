5756b76260ba054031ed8e457c253c520a9ecc21
Mark Calvelo <combatdstar@gmail.com>
2019-08-28 16:00:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(line) {
   let prompt = `The line is currently:`;
   let i = 0
   while (i < line.length) {
-    prompt += ` ${i}. ${line[i]}`;
+    prompt += ` ${i+1}. ${line[i]}`;
     i++;
   }
   prompt += '.';
